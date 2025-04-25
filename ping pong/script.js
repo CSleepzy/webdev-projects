bde6362426e594d34fca468058ffcfc84d8e6b17
CSleepzy <csleepzy@gmail.com>
2025-04-24 18:37:40 -0700

    push changes

--- ping pong/script.js
+++ ping pong/script.js
@@ -3,10 +3,10 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = 150;
 canvas.height = 150;
-ctx.beginPath()
-ctx.fillStyle = "red";
-ctx.fillRect(75, 75, 50, 30);
-ctx.closePath()
+// ctx.beginPath()
+// ctx.fillStyle = "red";
+// ctx.fillRect(75, 75, 50, 30);
+// ctx.closePath()
 x_vel = 2
 y_vel = 2
 var ball_size = 20
@@ -15,8 +15,8 @@ var paddle_height = 10
 var paddle_color = "purple"
 var ball_color = "white"
 var position_paddle = 0
-var ball_x = 0
-var ball_y = 0
+var ball_x = 50
+var ball_y = 50
 var paddle_left_x = 100
 var paddle_left_y = 250
 var paddle_right_x = 700
@@ -24,6 +24,11 @@ var paddle_right_y = 250
 createPaddle(paddle_width, paddle_height, paddle_color, paddle_left_x, paddle_left_y)
 createPaddle(paddle_width, paddle_height, paddle_color, paddle_right_x, paddle_right_y)
 createBall(ball_x, ball_y, ball_color, ball_size)
+function animate(){
+    moveBall()
+    createBall(ball_x, ball_y, ball_color, ball_size)
+    window.requestAnimationFrame(animate)
+}
 function createPaddle(w, h, c, x, y){
     ctx.beginPath()
     ctx.rect(x, y, w, h)
@@ -38,5 +43,12 @@ function createBall(x, y, c, r){
     ctx.fill()
 }
 function moveBall() {
-    if ()
+    if (ball_x + x_vel > canvas.width || ball_x + x_vel < 0) {
+        x_vel *= -1
+    }
+    if (ball_y + y_vel > canvas.height || ball_y + y_vel < 0){
+        y_vel *= -1
+    }
+    ball_x += x_vel
+    ball_y += y_vel
 }
