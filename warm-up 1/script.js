05b8d3b1df734b34add4a84c514e2733e03a3611
CSleepzy <csleepzy@gmail.com>
2025-04-24 17:50:11 -0700

    codeeeeeeee

--- warm-up 1/script.js
+++ warm-up 1/script.js
@@ -1,10 +1,16 @@
 var btn = document.getElementById('b1');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-
 canvas.width = 150;
 canvas.height = 150;
 ctx.beginPath()
 ctx.fillStyle = "red";
 ctx.fillRect(75, 75, 50, 30);
 ctx.closePath()
+
+var circle_diameter = 20
+var width_paddle = 15
+var height_paddle = 100
+var color_paddle = "purple"
+var color_circle = "white"
+var position_paddle = 0
